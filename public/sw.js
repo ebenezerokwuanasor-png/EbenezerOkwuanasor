472cef50c3da8c0dbefc02de7e34d3088be99062
Okwuanasor Ebenezer Onyekachukwu <ebenezerokwuanasor@gmail.com>
2026-07-15 11:10:08 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,82 @@
+const CACHE_NAME = "ebenezer-portfolio-v2";
+const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+const IMAGE_HOSTS = new Set(["raw.githubusercontent.com"]);
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(request.url);
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            const responseCopy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put("/", responseCopy));
+          }
+
+          return response;
+        })
+        .catch(() => caches.match("/").then((cachedResponse) => cachedResponse || caches.match("/index.html"))),
+    );
+    return;
+  }
+
+  if (IMAGE_HOSTS.has(requestUrl.hostname)) {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response.ok) {
+            const responseCopy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, responseCopy));
+          }
+
+          return response;
+        })
+        .catch(() => caches.match(request).then((cachedResponse) => cachedResponse || Response.error())),
+    );
+    return;
+  }
+
+  if (requestUrl.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(request).then((response) => {
+          if (response.ok) {
+            const responseCopy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, responseCopy));
+          }
+
+          return response;
+        });
+      }),
+    );
+  }
+});
